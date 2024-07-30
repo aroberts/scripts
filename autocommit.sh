86a49610256652bcd2bb02719e2b1f35243dea63
Andrew Roberts <adroberts@gmail.com>
2024-07-30 15:34:55 -0400

    added debug capability to autocommit- still needs full disk access to be scheduled with launchd though

--- autocommit.sh
+++ autocommit.sh
@@ -2,6 +2,12 @@
 
 tgt=$1
 
+debug() {
+  if [ "$DEBUG" != "" ]; then
+    echo >&2 "DEBUG: $1"
+  fi
+}
+
 usage() {
   echo >&2 "usage: $0 <git-rootdir-name>"
   exit 1
@@ -10,11 +16,15 @@ usage() {
 [ "$#" -eq 1 ] || usage
 [ -d "$tgt/.git" ] || usage
 
+debug "Entering directory"
+
 cd "$tgt"
 git="/usr/local/bin/git"
 
+debug "Checking git status"
+
 if [[ -n $($git status --short) ]]; then
-  $git add -A && git commit -m "Autocommit `date`"
+  debug "Running git add" && $git add -A && debug "Running git commit" && git commit -m "Autocommit `date`"
 else
-  echo 'clean'
+  debug 'clean'
 fi
